ccc28d338b8b8c3f26bc01fdb0c3e558275a1266
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-27 20:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+
+   
+
+
+        function updateTimer() {
+            const timerElement = document.getElementById('timer');
+            const now = new Date();
+            const options = {  year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+            const formattedDate = now.toLocaleDateString('en-US', options);
+            timerElement.textContent = formattedDate;
+        }
+
+        // Call the updateTimer function initially to display the time immediately.
+        updateTimer();
+
+        // Update the timer every second
+        setInterval(updateTimer, 1000);
